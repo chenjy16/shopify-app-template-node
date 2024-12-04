9eae24023df4147bccbd1cfa353f54934a5851d2
jay <jay.chen@castlery.com>
2024-12-04 13:12:01 +0800

    adjust review

--- web/fetchProducts.js
+++ web/fetchProducts.js
@@ -3,24 +3,12 @@ import shopify from "./shopify.js";
 
 // GraphQL 查询语句
 const ProductQuery = `
-  query GetProducts($query: String!) {
+  query GetProducts($query: String) {
     products(first: 10, query: $query) {
       edges {
         node {
           id
           title
-          description
-          featuredImage {
-            originalSrc
-          }
-          variants(first: 10) {
-            edges {
-              node {
-                id
-                title
-              }
-            }
-          }
         }
       }
     }
@@ -31,7 +19,11 @@ export const fetchProducts = async (session, query = "") => {
   const client = new shopify.api.clients.Graphql({ session });
 
   try {
-    const response = await client.request(ProductQuery, { query });
+    const response = await client.request(ProductQuery, {
+      variables: {
+        query: query,
+      },
+    });
     const products = response.products.edges.map(({ node }) => ({
       id: node.id,
       title: node.title,
